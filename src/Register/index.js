50a3d44a4c764e1ecaf6e13a163c8d256fa9befd
wdl <weidongliang9@sjtu.edu.cn>
2024-02-16 18:31:58 +0800

    add shuomingshu
    Signed-off-by: wdl <weidongliang9@sjtu.edu.cn>

--- src/Register/index.js
+++ src/Register/index.js
@@ -7,19 +7,22 @@ function Register() {
     const navigate = useNavigate()
     const [code, setCode] = useState("")
     const [isSending, setIsSending] = useState(false)
+    const [isChecked, setIsChecked] = useState(false)
 
     const handleSendingCode = async () => {
         const email = document.querySelector('.txtemail').value
-        const generatedCode = String(Math.floor(Math.random() * 1000000)).padEnd(6, '0')
-        setCode(generatedCode)
-        setIsSending(true)
-        fetch('https://todo-nodejs-nu.vercel.app/api/email', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email:email, code:generatedCode }),
-            });
+        if (email){
+            const generatedCode = String(Math.floor(Math.random() * 1000000)).padEnd(6, '0')
+            setCode(generatedCode)
+            setIsSending(true)
+            fetch('https://todo-nodejs-nu.vercel.app/api/email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email:email, code:generatedCode }),
+                });
+        }
     }
 
     const handleRegister = () => {
@@ -28,7 +31,7 @@ function Register() {
         const passWord = document.querySelector('.txtpassword').value;
         const canvasUrl = document.querySelector('.txtcanvasurl').value;
         const email = document.querySelector('.txtemail').value;
-        if (enteredCode === code && userName && passWord && canvasUrl && email && isSending) {
+        if (enteredCode === code && userName && passWord && canvasUrl && email && isSending && isChecked) {
           fetch('https://todo-nodejs-nu.vercel.app/insert-user', {
             method: 'POST',
             headers: {
@@ -54,7 +57,7 @@ function Register() {
         <div className='col-12 register'>
             <h3>注册</h3>
             <br></br>
-            <div className='col-6'>
+            <div className='col-9'>
                 <div class="form-outline mb-4">
                     <label class="form-label username" for="form2Example1">用户</label>
                     <input name='userName' type="text" id="form2Example1" class="form-control txtusername" />
@@ -62,18 +65,48 @@ function Register() {
 
                 <div class="form-outline mb-4">
                     <label class="form-label password" for="form2Example2">密码</label>
-                    <span class="question-mark" data-toggle="tooltip" data-placement="top" title="不要用你平时常用的密码,因为wdl在后台能看到">
-                        <i class="fas fa-question-circle"></i>
-                    </span>
+                    <i class="fas fa-question-circle" data-bs-toggle="modal" data-bs-target="#passwordModal"></i>
                     <input name='passWord' type="password" id="form2Example2" class="form-control txtpassword" />
+                </div>
+
+                <div class="modal fade" id="passwordModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">温馨提示</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            尽量不要用你平时常用的密码，因为我不太能保证网站的安全性
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">我知道了</button>
+                        </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="form-outline mb-4">
                     <label class="form-label canvasUrl" for="form2Example3">canvas日历订阅</label>
-                    <span class="question-mark" data-toggle="tooltip" data-placement="top" title="登录canvas,进入日历视图,点击蓝色的那个“日历订阅”,复制链接">
-                        <i class="fas fa-question-circle"></i>
-                    </span>
+                        <i class="fas fa-question-circle" data-bs-toggle="modal" data-bs-target="#canvasModal"></i>
                     <input name='canvasUrl' type="text" id="form2Example3" class="form-control txtcanvasurl" />
+                </div>
+
+                <div class="modal fade" id="canvasModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">如何订阅canvas日历?</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            登录canvas ，进入日历视图，点击蓝色的那个“日历订阅”，复制链接到这里来
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">我知道了</button>
+                        </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="form-outline mb-4">
@@ -84,6 +117,41 @@ function Register() {
                 <div class="form-outline mb-4">
                     <label class="form-label code" for="form2Example5">验证码</label>
                     <input type="text" id="form2Example5" class="form-control txtcode" />
+                </div>
+
+                <div class="row mb-4">
+                    <div class="col d-flex justify-content-center">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" checked={isChecked}  onChange={() => setIsChecked(!isChecked)}/>
+                            <label class="form-check-label"> 我已阅读 </label> 
+                            <span className="highlight" data-bs-toggle="modal" data-bs-target="#detailModal">说明书</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">说明书</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p>1. 同步canvas任务和同步教务处通知，点击按钮后，请等待至少7秒，然后手动刷新页面。</p>
+                            <p>2. 请不要频繁地使用同步教务处通知的功能，万一把教务处网站搞崩溃就麻烦了。</p>
+                            <p>3. 任务展示是按照deadLine时间排序的，记录展示是按添加/发布时间排序的。</p>
+                            <p>4. 同步教务处通知时，会添加教务处最新的10条通知。不会重复添加，但如果你删掉过这10条之一的通知，它也会再被添加上。
+                                因此，我建议你在“非精选”一栏里保留10条来自教务处的通知，然后超过这10条的不重要的通知都可以删掉了。
+                            </p>
+                            <p>5. 不要存太多不重要的记录，不然我数据库内存不够。</p>
+                            <p>6. 点击任务/记录标题可以进入详情页面。刚同步的教务处通知的细节部分是有所省略的，可以点击“更新”按钮来获取完整内容。也请不要频繁地使用更新功能。</p>
+                            <p>7. 一切解释权归wdl同学所有，不懂就问。</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">我知道了</button>
+                        </div>
+                        </div>
+                    </div>
                 </div>
                 
                 <div className='btns-area'>
